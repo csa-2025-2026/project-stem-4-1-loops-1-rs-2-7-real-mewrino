18e9d98dbffb468ce2ff6a45cb842ce20a1b1b67
mewrino <michellenguyen04272008@gmail.com>
2025-10-17 21:46:15 +0000

    r

--- Main.java
+++ Main.java
@@ -4,6 +4,26 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner scan = new Scanner(System.in);
+    // Problem 1 System.out.println("Enter numbers to add. Enter -1 to stop");
+    
+    /* int num = 0;
+    int sum = 0;
+    while (num != -1)
+      {
+        num = scan.nextInt();
+        sum += num;
+      }
+    sum++;
+    System.out.println("The sum is: " + sum);
+
+    scan.close(); */
+
+    // Problem 2
+    System.out.println("How many numbers do you wish to enter?");
+    
+
+
+
   }
 }
